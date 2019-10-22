cb3002edf7d4919c6239ad02d5a989b0134b2bbe
Ferced <fcedeirajoaquin@gmail.com>
2019-10-22 16:08:43 -0300

    added image carousel

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import Header from './components/layout/Header';
-import Footer from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+ 
+
 import './App.css';
 
 function App() {
@@ -11,6 +15,23 @@ function App() {
     <div className="App">
       <div className="container">
         <Header />
+        <Carousel autoplay={true} interval= {500} showArrows={false} showIndicators={false} showStatus= {false} showThumbs = {false} infiniteLoop = {true}>
+                <div>
+                    <img
+                    src="https://g.foolcdn.com/image/?url=https%3A%2F%2Fg.foolcdn.com%2Feditorial%2Fimages%2F540461%2Fexplaining-why-stock-going-up-2018_05_10-12_39_51-utc.jpg&w=700&op=resize" 
+                    alt="new"/>
+                </div>
+                <div>
+                <img
+                  src="https://g.foolcdn.com/image/?url=https%3A%2F%2Fg.foolcdn.com%2Feditorial%2Fimages%2F540461%2Fexplaining-why-stock-going-up-2018_05_10-12_39_51-utc.jpg&w=700&op=resize" 
+                  alt="new"/>
+                </div>
+                <div>
+                <img
+                     src="https://g.foolcdn.com/image/?url=https%3A%2F%2Fg.foolcdn.com%2Feditorial%2Fimages%2F540461%2Fexplaining-why-stock-going-up-2018_05_10-12_39_51-utc.jpg&w=700&op=resize" 
+                    alt="new"/>
+                </div>
+            </Carousel>
 <p>sarasa</p>     
 <p>sarasa</p>     
 <p>sarasa</p>     
