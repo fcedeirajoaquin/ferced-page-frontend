33e65596d4ce1d68f609f0a4d5c1ac43c912d6e5
Ferced <fcedeirajoaquin@gmail.com>
2019-11-05 17:42:24 -0300

    made the Header a class component instead of a function

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,41 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Header() {
-  var imageShadowStyle = {
+class Header extends Component {
+  imageShadowStyle = {
 filter: 'brightness(0%)',
     height: "60px",
     marginLeft:"-5px",
 position:'absolute',
 zIndex:0
   }
-var imageStyle = {
+imageStyle = {
     height: "60px",
 position:'absolute',
 zIndex:1
   }
-  return (
-    <header style={headerStyle}>
-      <img style={imageStyle} src={process.env.PUBLIC_URL + "/ourLogos/fercedLogo.png"}/>
-      <img style={imageShadowStyle } src={process.env.PUBLIC_URL + "/ourLogos/fercedLogo.png"}/>
-   </header>
-  )
+ headerStyle = {
+    backgroundColor: "rgba(0,0,0,0.5)",
+    color: '#fff',
+    height:'55px',
+    textAlign:'center',
+    alignContent:"center",
+  position:"absolute",
+  width:"100%",
+  zIndex:3
+  }
+  render() {
+return(
+  <header style={this.headerStyle}>
+  <img style={this.imageStyle} src={process.env.PUBLIC_URL + "/ourLogos/fercedLogo.png"}/>
+  <img style={this.imageShadowStyle } src={process.env.PUBLIC_URL + "/ourLogos/fercedLogo.png"}/>
+</header>
+
+)
+  }
+
 }
 
-const headerStyle = {
-  backgroundColor: "rgba(0,0,0,0.5)",
-  color: '#fff',
-  height:'55px',
-  textAlign:'center',
-  alignContent:"center",
-position:"absolute",
-width:"100%",
-zIndex:3
-}
 
-const linkStyle = {
-  color: '#fff',
-  textDecoration: 'none'
-}
 
 export default Header;
