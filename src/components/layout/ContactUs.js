9a0847b031f8768fbbe306d8d09f0cadf3f275aa
Ferced <fcedeirajoaquin@gmail.com>
2019-11-13 17:27:37 -0300

    fixed some icons in ContactUs

--- src/components/layout/ContactUs.js
+++ src/components/layout/ContactUs.js
@@ -3,9 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
-import AccountCircleSharpIcon from '@material-ui/icons/AccountCircleSharp';
+import EmailSharp from '@material-ui/icons/EmailSharp';
 import Grid from '@material-ui/core/Grid';
-import AccountCircle from '@material-ui/icons/AccountCircle';
+import {MessageSharp,PhoneSharp} from '@material-ui/icons';
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
 import {
@@ -29,14 +29,13 @@ export default function ContactUs () {
 
   const classes = useStyles();
       return (
-       
          <div style={{paddingBottom:"60px"}}>
-         <div style={{ background: "white", height: "50%", textAlign: "center", marginTop: "50px", marginBottom: "2px" }}>
-         <p style={{ color: "#64B5F6", fontWeight: "400", fontSize: "2.92rem", lineHeight: "110%", margin: "1.9466666667rem 0 1.168rem 0", fontFamily: "apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif" }}>
+          <div style={{ background: "white", height: "50%", textAlign: "center", marginTop: "50px", marginBottom: "2px" }}>
+          <p style={{ color: "#64B5F6", fontWeight: "400", fontSize: "2.92rem", lineHeight: "110%", margin: "1.9466666667rem 0 1.168rem 0", fontFamily: "apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif" }}>
            Contacto
-         </p>
+          </p>
           
-         <p style={{ paddingLeft: "60px", paddingRight: "60px", paddingBottom: "30px", fontFamily: "apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif", fontWeight: "normal", color: "rgba(0, 0, 0, 0.87)", display: "block", textAlign: "center", fontSize: "14px", lineHeight: "2rem" }}>
+          <p style={{ paddingLeft: "60px", paddingRight: "60px", paddingBottom: "30px", fontFamily: "apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif", fontWeight: "normal", color: "rgba(0, 0, 0, 0.87)", display: "block", textAlign: "center", fontSize: "14px", lineHeight: "2rem" }}>
            Dejanos tus datos y nosotros te contactamos
        </p>
        </div>
@@ -47,7 +46,7 @@ export default function ContactUs () {
  <div className={classes.margin}>
         <Grid container spacing={1} alignItems="flex-end">
           <Grid item>
-            <AccountCircle />
+            <AccountCircleSharpIcon />
           </Grid>
           <Grid item>
             <TextField id="input-with-icon-grid" label="With a grid" />
@@ -57,7 +56,7 @@ export default function ContactUs () {
       <div className={classes.margin}>
         <Grid container spacing={1} alignItems="flex-end">
           <Grid item>
-            <AccountCircle />
+            <PhoneSharp />
           </Grid>
           <Grid item>
             <TextField id="input-with-icon-grid" label="With a grid" />
@@ -72,7 +71,7 @@ export default function ContactUs () {
  <div className={classes.margin}>
         <Grid container spacing={1} alignItems="flex-end">
           <Grid item>
-            <AccountCircle />
+          <EmailSharp />
           </Grid>
           <Grid item>
             <TextField id="input-with-icon-grid" label="With a grid" />
@@ -82,7 +81,7 @@ export default function ContactUs () {
       <div className={classes.margin}>
         <Grid container spacing={1} alignItems="flex-end">
           <Grid item>
-            <AccountCircle />
+            <MessageSharp />
           </Grid>
           <Grid item>
             <TextField id="input-with-icon-grid" label="With a grid" />
