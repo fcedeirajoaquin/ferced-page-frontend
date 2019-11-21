f3d004d2896dd6aeb66076042021e6bf186ecc2e
Ferced <fcedeirajoaquin@gmail.com>
2019-11-21 15:04:40 -0300

    Updates

---
+++ precache-manifest.793a430b50d53adba3359d1852325ded.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94e7ec91346da5c4f4fac3fa79eda7a0",
+    "url": "/ferced-page-frontend/index.html"
+  },
+  {
+    "revision": "5d822ab1e3165618606b",
+    "url": "/ferced-page-frontend/static/css/2.f997cfaa.chunk.css"
+  },
+  {
+    "revision": "a0484e1338c29b60809b",
+    "url": "/ferced-page-frontend/static/css/main.cdfc0761.chunk.css"
+  },
+  {
+    "revision": "5d822ab1e3165618606b",
+    "url": "/ferced-page-frontend/static/js/2.48f97b85.chunk.js"
+  },
+  {
+    "revision": "a0484e1338c29b60809b",
+    "url": "/ferced-page-frontend/static/js/main.f9c48210.chunk.js"
+  },
+  {
+    "revision": "c1f42296acdbbe8bcfb2",
+    "url": "/ferced-page-frontend/static/js/runtime-main.75b46795.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/ferced-page-frontend/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/ferced-page-frontend/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/ferced-page-frontend/static/media/revicons.2feb69cc.eot"
+  }
+]);
