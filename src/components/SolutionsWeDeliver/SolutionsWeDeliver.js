4502f379a45a8ed24e944dcbbd3438a4f73217a1
smeraldgreen <frxnco@protonmail.com>
2021-03-22 23:51:28 -0300

    fix screen solutionwedeliver

--- src/components/SolutionsWeDeliver/SolutionsWeDeliver.js
+++ src/components/SolutionsWeDeliver/SolutionsWeDeliver.js
@@ -14,22 +14,22 @@ class Solutions extends Component {
 
             <div style={{  background:  "url(https://i.pinimg.com/originals/b9/a2/9a/b9a29a075ec4beb68025515f63432880.gif)", backgroundSize:"cover", 
                 color:"white",  height:"100vh"}}>
-                <div style={{backgroundColor:"black", opacity:"0.8", height:"100vh"}}>
+                <div style={{backgroundColor:"black", opacity:"0.8", height:"100%"}}>
                     <Row xs="2" style={{display: 'flex',  justifyContent:'center', alignItems:'center',width:"100%", height:"100%"}} >
                         <Fade left duration="5000">
                             <Col>
-                            <MaterialIcon icon="autorenew" size='large' color="pink"/><p style={{fontSize:"30px"}}> Business Processes Automation</p>
+                            <MaterialIcon icon="autorenew" size='large' color="pink"/><p style={{fontSize:"20px"}}> Business Processes Automation</p>
                             </Col>
                             <Col>
-                            <MaterialIcon icon="code" size='large' color="green"/><p style={{fontSize:"30px"}}> Web y Mobile applications</p>
+                            <MaterialIcon icon="code" size='large' color="green"/><p style={{fontSize:"20px"}}> Web y Mobile applications</p>
                             </Col>
                         </Fade>
                         <Fade right duration="5000">
                         <Col>
-                            <MaterialIcon icon="integration_instructions" size='large' color="violet"/><p style={{fontSize:"30px"}}> Integrations</p>
+                            <MaterialIcon icon="integration_instructions" size='large' color="violet"/><p style={{fontSize:"20px"}}> Integrations</p>
                             </Col>
                             <Col>
-                            <MaterialIcon icon="multiple_stop" size='large' color="yellow"/><p style={{fontSize:"30px"}}> Scaling Product Delivery</p>
+                            <MaterialIcon icon="multiple_stop" size='large' color="yellow"/><p style={{fontSize:"20px"}}> Scaling Product Delivery</p>
                             </Col>
                         </Fade>
                     
