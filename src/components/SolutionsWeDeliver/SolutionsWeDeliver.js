2ba9c63e6c2c779edd82d9ea9335df5446c474ff
smeraldgreen <frxnco@protonmail.com>
2021-03-22 23:25:33 -0300

    Screen "solutionwedelivery" now with new style

--- src/components/SolutionsWeDeliver/SolutionsWeDeliver.js
+++ src/components/SolutionsWeDeliver/SolutionsWeDeliver.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
 import {Col, Row} from 'reactstrap';
 import MaterialIcon, {colorPalette} from 'material-icons-react';
-import Jump from 'react-reveal/Jump';
+import Pulse from 'react-reveal/Pulse';
+import Fade from 'react-reveal/Fade';
 
 class Solutions extends Component {    
 
 
     render() {
         return (
-        <Row xs="2" style={{marginTop:"100px", width:"100%", height:"100%"}}>
-            <Jump left>
-            <Col>
-            <MaterialIcon icon="autorenew" size='large'/><p style={{fontSize:"30px"}}> Business Processes Automation</p>
-            </Col>
-            <Col>
-            <MaterialIcon icon="code" size='large'/><p style={{fontSize:"30px"}}> Web y Mobile applications</p>
-            </Col>
-            <Col>
-            <MaterialIcon icon="integration_instructions" size='large'/><p style={{fontSize:"30px"}}> Integrations</p>
-            </Col>
-            <Col>
-            <MaterialIcon icon="multiple_stop" size='large'/><p style={{fontSize:"30px"}}> Scaling Product Delivery</p>
-            </Col>
-            </Jump>
-        </Row>
+      
+
+            <div style={{backgroundColor:"grey", color:"white",  height:"100vh"}}>
+                <Row xs="2" style={{display: 'flex',  justifyContent:'center', alignItems:'center',width:"100%", height:"100%"}} >
+                    <Fade left duration="5000">
+                        <Col>
+                        <MaterialIcon icon="autorenew" size='large' color="pink"/><p style={{fontSize:"30px"}}> Business Processes Automation</p>
+                        </Col>
+                        <Col>
+                        <MaterialIcon icon="code" size='large' color="yellow"/><p style={{fontSize:"30px"}}> Web y Mobile applications</p>
+                        </Col>
+                    </Fade>
+                    <Fade right duration="5000">
+                    <Col>
+                        <MaterialIcon icon="integration_instructions" size='large' color="violet"/><p style={{fontSize:"30px"}}> Integrations</p>
+                        </Col>
+                        <Col>
+                        <MaterialIcon icon="multiple_stop" size='large' color="blue"/><p style={{fontSize:"30px"}}> Scaling Product Delivery</p>
+                        </Col>
+                    </Fade>
+                
+                </Row>
+            </div> 
         )
     }
 }
